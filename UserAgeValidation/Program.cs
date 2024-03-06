7d1e6501d7af3259ef27d56ff5f199c3502eb332
Tormtormiline <toivo.luhaaar@tktk.ee>
2024-03-06 23:28:36 +0200

    first commit

---
+++ UserAgeValidation/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Enter your age:");
+
+int userAge = Int32.Parse(Console.ReadLine()); // "13" - heap, "13" - stack
+
+if (userAge >= 13)
+{
+    Console.WriteLine("Welcome to Instagram!");
+}
+
+else //if (userAge < 13) ;
+{
+    Console.WriteLine("You are too young to use Instagram. ");
+}
+
+
---
+++ UserGenderValidation/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// rakendus küsib kasutajalt tema sugu  (m/f)
+//rakendus küsib kasutajalt sisestada oma perekonnanimi 
+//lähtudes kasutaja valikust, rakendus tervitab kasutajat järgmiselt:
+//"Welcome, Mr. [kasutaja perekonnanimi] / "Welcome, Ms. [kasutaja perekonnanimi]"
+
+Console.WriteLine("Please, select your gender (m/f):");
+
+char userGender = char.Parse(Console.ReadLine()); // loeb konsoolist maha andmeid string (sõna) formaadis
+
+Console.WriteLine("Please, enter your last name:");
+string userLastName = Console.ReadLine();
+
+
+if (userGender == 'm')
+{
+    Console.WriteLine($"Welcome, Mr. {userLastName}!");
+}
+
+else if (userGender == 'f') ;
+{
+    Console.WriteLine($"Welcome, M. {userLastName} !");
+}
+
+
+
